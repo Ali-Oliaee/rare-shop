3bea68ff5183cda5ba810791be2d7d2a73bd3a45
ali <alioliaee31@gmail.com>
2022-08-08 17:39:45 +0430

    improve styles

--- frontend/src/components/User/Layout/index.js
+++ frontend/src/components/User/Layout/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import UserHeader from '../Header'
 import Footer from '../Footer/Footer'
+import './style.scss'
 
 function UserLayout({ children }) {
   return (
-    <>
+    <div className="user-layout">
       <UserHeader />
-      <div>{children}</div>
+      <div className="content">
+        {children}
+      </div>
       <Footer />
-    </>
+    </div>
   )
 }
 
