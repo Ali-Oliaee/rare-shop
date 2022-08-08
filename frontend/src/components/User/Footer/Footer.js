66a44a53f4f7b8a22c2240420d4c8a38463af1cc
ali <alioliaee31@gmail.com>
2022-08-08 14:34:37 +0430

    change footer

--- frontend/src/components/User/Footer/Footer.js
+++ frontend/src/components/User/Footer/Footer.js
@@ -1,30 +1,14 @@
-import React from 'react'
-import styled from 'styled-components'
+import { Button } from '@mui/material'
+import { Link } from 'react-router-dom'
+import './style.scss'
 
-const MyFooter = styled('footer')`
-   width: 100%;
-   height: 30vh;
-   background: #90dbf4;
-`
-const MyUl = styled('ul')`
-   margin: 0;
-   list-style: none;
-   padding: 2rem;
-`
-const MyLi = styled('li')`
-   text-decoration: none;
-   color: #fbff12;
-   font-size: 18px;
-`
 function Footer() {
   return (
-    <MyFooter>
-      <MyUl>
-        <MyLi>About Us</MyLi>
-        <MyLi>Contact Us</MyLi>
-        <MyLi>Stores</MyLi>
-      </MyUl>
-    </MyFooter>
+    <div className="footer">
+      <Button linkComponent={Link} to="/about">About</Button>
+      <Button linkComponent={Link} to="/contact">Contact</Button>
+      <Button linkComponent={Link} to="/stores">Stores</Button>
+    </div>
   )
 }
 
