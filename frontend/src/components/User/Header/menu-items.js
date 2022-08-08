d304c3f8d150f3ffcb862e0ad9bef3b9893a89d6
ali <alioliaee31@gmail.com>
2022-08-08 13:55:28 +0430

    lint codes

--- frontend/src/components/User/Header/menu-items.js
+++ frontend/src/components/User/Header/menu-items.js
@@ -1,54 +1,18 @@
 const menuItems = {
   پوشاک: [
-    {
-      title: 'شلوار',
-      path: '/products/category/1',
-      cName: 'dropdown-link',
-    },
-    {
-      title: 'تیشرت و شومیز',
-      path: '/products/category/1',
-      cName: 'dropdown-link',
-    },
-    {
-      title: 'پیراهن',
-      path: '/products/category/1',
-      cName: 'dropdown-link',
-    },
+    { title: 'شلوار', path: '/products/category/1' },
+    { title: 'تیشرت و شومیز', path: '/products/category/1' },
+    { title: 'پیراهن', path: '/products/category/1' },
   ],
   'کیف و کفش': [
-    {
-      title: 'کفش استپرت',
-      path: '/products/category/2',
-      cName: 'dropdown-link',
-    },
-    {
-      title: 'کفش مجلسی',
-      path: '/products/category/2',
-      cName: 'dropdown-link',
-    },
-    {
-      title: 'کیف',
-      path: '/products/category/2',
-      cName: 'dropdown-link',
-    },
+    { title: 'کفش استپرت', path: '/products/category/2' },
+    { title: 'کفش مجلسی', path: '/products/category/2' },
+    { title: 'کیف', path: '/products/category/2' },
   ],
   اکسسوری: [
-    {
-      title: 'عینک',
-      path: '/products/category/3',
-      cName: 'dropdown-link',
-    },
-    {
-      title: 'گردنبند و گوشواره',
-      path: '/products/category/3',
-      cName: 'dropdown-link',
-    },
-    {
-      title: 'انگشتر و دستبند',
-      path: '/products/category/3',
-      cName: 'dropdown-link',
-    },
+    { title: 'عینک', path: '/products/category/3' },
+    { title: 'گردنبند و گوشواره', path: '/products/category/3' },
+    { title: 'انگشتر و دستبند', path: '/products/category/3' },
   ],
 }
 
