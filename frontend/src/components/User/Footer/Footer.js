023dc1b034048f1efb6e99377b9501347122eb3e
james <ali_o@felixin.io>
2022-09-20 23:35:54 +0430

    change footer content

--- frontend/src/components/User/Footer/Footer.js
+++ frontend/src/components/User/Footer/Footer.js
@@ -1,15 +1,21 @@
-import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
-import './style.scss'
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import './style.scss';
 
 function Footer() {
   return (
     <div className="footer">
-      <Button component={Link} to="/about">About</Button>
-      <Button component={Link} to="/contact">Contact</Button>
-      <Button component={Link} to="/stores">Stores</Button>
+      <Button component={Link} to="/about">
+        درباره ما
+      </Button>
+      <Button component={Link} to="/contact">
+        تماس با ما
+      </Button>
+      <Button component={Link} to="/stores">
+        شعب
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
